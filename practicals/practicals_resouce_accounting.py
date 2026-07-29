18c448902e7cb9e93348cc1391c56e140c5b44ae
Soumya Banerjee <neel.soumya@gmail.com>
2026-07-29 17:45:54 +0100

    edits to week 1to 4

---
+++ practicals/practicals_resouce_accounting.py
@@ -0,0 +1,15 @@
+'''
+Practical for Resource Accounting
+'''
+
+def get_memory_usage(x: torch.Tensor):
+    return x.numel() * x.element_size()
+
+import torch
+x = torch.zeros(4, 8)  
+assert x.dtype == torch.float32  # Default type
+assert x.numel() == 4 * 8
+assert x.element_size() == 4  # Float is 4 bytes
+assert get_memory_usage(x) == 4 * 8 * 4  # 128 bytes
+# One matrix in the feedforward layer of GPT-3:
+assert get_memory_usage(torch.empty(12288 * 4, 12288)) == 2304 * 1024 * 1024  # 2.3 GB 
