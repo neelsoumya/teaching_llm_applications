1bcf26b7cb17128ea092483eae2afc20b96b5103
Soumya Banerjee <neel.soumya@gmail.com>
2026-08-20 10:04:56 +0100

    edits to new material

--- practicals/practical_transformers.py
+++ practicals/practical_transformers.py
@@ -7,5 +7,7 @@ Adapted from:
     https://colab.research.google.com/github/tensorflow/text/blob/master/docs/tutorials/transformer.ipynb
 
     https://github.com/tensorflow/text/blob/master/docs/tutorials/transformer.ipynb
+
+    https://github.com/neelsoumya/teaching_llm_applications/blob/main/practicals/transformer.ipynb
     
 '''
