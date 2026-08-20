dc493d2c3538cc09c6180094327ca5f68c861151
Soumya Banerjee <neel.soumya@gmail.com>
2026-08-20 10:02:14 +0100

    edits to new material

---
+++ practicals/practical_transformers.py
@@ -0,0 +1,11 @@
+'''
+Practical on transformers
+
+Adapted from:
+    https://www.tensorflow.org/text/tutorials/transformer
+
+    https://colab.research.google.com/github/tensorflow/text/blob/master/docs/tutorials/transformer.ipynb
+
+    https://github.com/tensorflow/text/blob/master/docs/tutorials/transformer.ipynb
+    
+'''
