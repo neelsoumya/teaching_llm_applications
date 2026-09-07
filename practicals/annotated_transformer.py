896aece28bcf5834591b953c9bb85d8921cd8db6
Soumya Banerjee <neel.soumya@gmail.com>
2026-09-07 11:41:32 +0100

    edits to new material

--- practicals/annotated_transformer.py
+++ practicals/annotated_transformer.py
@@ -1,6 +1,10 @@
 '''
 ## Code for transformer architecture (from scratch)
 from https://nlp.seas.harvard.edu/annotated-transformer/
+
+Code also available in this google colab notebook:
+https://colab.research.google.com/drive/1uaPUeYVwvgc3oCZzOK4Zgb1qEkcrVsH7?usp=sharing
+
 '''
 
 # !pip install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl numpy matplotlib spacy torchtext seaborn
